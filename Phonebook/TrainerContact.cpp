0810f18ca50af2c98732c07969dd87c844d43752
Blane Zewdie <b1ane@outlook.com>
2020-11-23 13:13:36 -0600

    file complete

--- Phonebook/TrainerContact.cpp
+++ Phonebook/TrainerContact.cpp
@@ -18,31 +18,37 @@ TrainerContact::TrainerContact(const string &_name, const string &_specialize, c
 
 string TrainerContact::getName()
 {
+    return this -> name;
    
 }
 
 string TrainerContact::getSpecialize()
 {
+    return this -> specialize;
    
 }
 
 void TrainerContact::setName(const string &_name)
 {
+    this -> name = _name;
   
 }
 
 void TrainerContact::setSpecialize(const string &_specialize)
 {
+    this -> specialize = _specialize;
   
 }
 
 void TrainerContact::print() const
 {
+    cout << "Name: " << this->name << ", Specialize: " << this->specialize << ", Region: " << this->getRegion() << ", Phone number: " << this->getPhoneNumber() << endl;
   
 }
 
 bool TrainerContact::operator!=(const GenericContact &contact) {
    // Write the implementation of the overloaded operator != here
+    return GenericContact::operator!=(contact);
 }
 
 TrainerContact::~TrainerContact() {}
