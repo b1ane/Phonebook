19997da9553a0ce5a68650b87ba8bf465aa4cb61
Blane Zewdie <b1ane@outlook.com>
2020-11-23 13:10:51 -0600

    file complete

--- Phonebook/GymContact.cpp
+++ Phonebook/GymContact.cpp
@@ -14,35 +14,43 @@ GymContact::GymContact() : GenericContact(), gName(""), badge("")
 
 GymContact::GymContact(const string &_gName, const string &_badge, const string &_region, const string &_phoneNumber) : GenericContact("G", _region, _phoneNumber), gName(_gName), badge(_badge)
 {
+    this->setGName(_gName);
+    this->setBadge(_badge);
 }
 
 string GymContact::getGName()
 {
    //complete
+    return this -> gName;
 }
 
 string GymContact::getBadge()
 {
     //complete
+    return this -> badge;
 }
 
 void GymContact::setGName(const string &_gName)
 {
     //complete
+    this -> gName = _gName;
 }
 
 void GymContact::setBadge(const string &_badge)
 {
     //complete
+    this -> badge = _badge;
 }
 
 void GymContact::print() const
 {
     //complete
+    cout << "Gym: " << this->gName << ", Badge: " << this->badge << ", Region: " << this->getRegion() << ", Phone number: " << this->getPhoneNumber() << endl;
 }
 
 bool GymContact::operator!=(const GenericContact &contact) {
    // Write the implementation of the overloaded operator != here
+    return GenericContact::operator!=(contact);
 }
 
 GymContact::~GymContact() {}
